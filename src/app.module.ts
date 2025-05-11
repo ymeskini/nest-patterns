df687f7792fbe85735e8fcfee22a52544e96b111
Youssef Meskini <youss.meskini@gmail.com>
2025-05-11 04:03:39 +0100

    init typeorm and use it form coffee entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // only for dev
+    }),
+  ],
   controllers: [],
   providers: [],
 })
